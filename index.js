24746161d9170d219803cff9722a65a1ffae5ee0
MaxKimGit <maxkim12@alumni.stanford.edu>
2025-07-16 10:30:16 -0700

    Make minor changes to scroll functionality and update fade in

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const eventScroll = document.querySelectorAll(".horizontal-scroll");
-for (card of eventScroll) {
-    card.addEventListener("wheel", (event) => {
-        event.preventDefault();
-        card.scrollBy({
-            left: event.deltaY < 0 ? -30 : 30,
-        });
-    });
+// const eventScroll = document.querySelectorAll(".horizontal-scroll");
+// for (card of eventScroll) {
+//     card.addEventListener("wheel", (event) => {
+//         event.preventDefault();
+//         card.scrollBy({
+//             left: event.deltaY < 0 ? -30 : 30,
+//         });
+//     });
+// }
+
+document.addEventListener("scroll", update);
+
+function update() {
+  const elem = document.querySelector(".horizontal-scroll");
+  const rect = elem.getBoundingClientRect();
+
+  if (rect.top < 530) {
+    const updates = document.querySelectorAll(".update-card");
+    for (card of updates) {
+        card.classList.add("fade-in")
+    }
+  }
 }
